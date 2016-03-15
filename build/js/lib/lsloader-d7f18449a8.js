0e01dd47f4eaf97d13dd8609ae1724278604cc51
yanghuanyu <yanghuanyu@meituan.com>
2016-03-15 20:38:19 +0800

    job done!

---
+++ build/js/lib/lsloader-d7f18449a8.js
@@ -0,0 +1,32 @@
+(function(){
+
+    window.lsloader = {};
+
+    //读取资源到模板中
+    lsloader.load = function(jsname,jspath){
+         var code = localStorage.getItem(jsname)
+         if(code){
+             document.getElementById(jsname).appendChild(document.createTextNode(code))
+         }else{
+             lsloader.io(jspath,function(code){
+                 document.getElementById(jsname).appendChild(document.createTextNode(code))
+                 localStorage.setItem(jsname,code);
+             })
+         }
+    }
+
+    lsloader.io = function(path,callback){
+        var xhr = new XMLHttpRequest()
+        xhr.open("get",path,true);
+        xhr.setRequestHeader("Content-Type","text/html")
+        xhr.onreadystatechange = function(){
+            if (xhr.readyState == 4) {//readyState表示文档加载进度,4表示完毕
+                callback(xhr.responseText);//responseText属性用来取得返回的文本
+            }
+        }
+        xhr.send(null);
+    }
+
+
+
+})()
--- gulptask/templateBuild.js
+++ gulptask/templateBuild.js
@@ -1,5 +1,6 @@
 //遍历模版文件中需要
-var path='build/html/'
+var path='build/html/' //要编译的路径
+var staticPath='../'   //要附加的静态路径
 
 var fs=require("fs");
 function compileFile(json){
@@ -17,50 +18,34 @@ function compileFile(json){
 }
 
 function replaceContent(content,json){
-    var cssOrginCodes = content.match(/<!--css ls build-->[\s|\S]*?<!--css ls endbuild-->/g)
-  //  console.log(cssOrginCodes)
-    var cssDistCodes = [];
-    for (var k in cssOrginCodes){
-        var insideHrefs = cssOrginCodes[k].match(/\w*\.css/g)
-        if(insideHrefs){ //match out href="test.css"
-           for(var i in insideHrefs ){
-               if(json[insideHrefs[i]]){
-                   cssOrginCodes[k]+='<script id="'+insideHrefs[i]+'">lsloader.loadCss('+insideHrefs[i]+','+json[insideHrefs[i]]+')</script>'
-               }
-           }
-        }
-        cssOrginCodes[k].replace(/<!--css ls build-->[\s|\S]*?<!--css ls endbuild-->/,'');
-    }
 
-    content.replace(/<!--css ls build-->[\s|\S]*?<!--css ls endbuild-->/g,function(cssOrginCodes){
+
+    content= content.replace(/<!--css ls build-->[\s|\S]*?<!--css ls endbuild-->/g,function(cssOrginCodes){
         var insideHrefs = cssOrginCodes.match(/\w*\.css/g)
         if(insideHrefs){ //match out href="test.css"
             for(var i in insideHrefs ){
                 if(json[insideHrefs[i]]){
-                    cssOrginCodes+='<script id="'+insideHrefs[i]+'">lsloader.loadCss('+insideHrefs[i]+','+json[insideHrefs[i]]+')</script>'
+                    cssOrginCodes+='<style id="'+insideHrefs[i].replace('.','_')+'"></style><script>lsloader.load("'+insideHrefs[i].replace('.','_')+'","'+staticPath+json[insideHrefs[i]]+'" )</script>'
                 }
             }
         }
         return cssOrginCodes.replace(/<!--css ls build-->[\s|\S]*?<!--css ls endbuild-->/,'');
     })
-    console.log(content)
 
 
-    //var preloadScript= content.match(/<script build=\'preload\'>[\s|\S]*<\/script>/)
-    //
-    //preloadScript= preloadScript[0].replace(/build=\'preload\'/,'');
-    //
-    //var staticArry= preloadScript.match(/\S*\.css|\S*\.js/g) //所有准备替换的静态资源
-    //
-    //var staticString = ''//替换后的静态地址
-    //for(var i in staticArry){
-    //    if(json[staticArry[i]]){
-    //        preloadScript = preloadScript.replace(new RegExp(staticArry[i],'g'),json[staticArry[i]]);
-    //    }
-    //}
-    //
-    //return content.replace(/<script build=\'preload\'>[\s|\S]*<\/script>/,preloadScript)
+    content= content.replace(/<!--js ls build-->[\s|\S]*?<!--js ls endbuild-->/g,function(cssOrginCodes){
+        var insideHrefs = cssOrginCodes.match(/\w*\.js/g)
+        if(insideHrefs){ //match out href="test.css"
+            for(var i in insideHrefs ){
+                if(json[insideHrefs[i]]){
+                    cssOrginCodes+='<script id="'+insideHrefs[i].replace('.','_')+'"></script><script>lsloader.load("'+insideHrefs[i].replace('.','_')+'","'+staticPath+json[insideHrefs[i]]+'" )</script>'
+                }
+            }
+        }
+        return cssOrginCodes.replace(/<!--js ls build-->[\s|\S]*?<!--js ls endbuild-->/,'');
+    })
 
+    return content
 
 }
 
